b09a3a9eccf4e715776110d86ca747237569b77b
Vrezerino <parkpatrick1@gmail.com>
2022-05-09 18:06:20 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import ladybug from './ladybug.png';
+import './App.css';
+import { useState } from 'react';
+const scream = require('./utils/scream').sound
+const pop = require('./utils/pop').sound
+
+const App = () => {
+	const winWidth = window.innerWidth;
+	const winHeight = window.innerHeight;
+
+	const getRandomPos = (min, max) => {
+		return Math.random() * (max - min - 50) + min;
+	}
+
+	const [randomTop, setRandomTop] = useState(getRandomPos(0, winHeight));
+	const [randomLeft, setRandomLeft] = useState(getRandomPos(0, winWidth));
+	const [fail, setFail] = useState(false); // Set to true when user fails to press/click on the ladybug in a given time.
+	const [time, setTime] = useState(3100); // 3.1 seconds is the initial time.
+	const [score, setScore] = useState(0); // Successful consecutive presses on the ladybug.
+	const [timer, setTimer] = useState(null); // Stored in state so it can be cleared.
+
+	const timeout = () => {
+		//console.log(time);
+		setTimer(setTimeout(() => {
+			setFail(true);
+		}, time));
+	}
+
+	const clicked = () => {
+		pop.play();
+		setScore(score + 1);
+		clearTimeout(timer);
+		setRandomTop(getRandomPos(0, winHeight));
+		setRandomLeft(getRandomPos(0, winWidth));
+
+		// After each click, user has 0.1 sec less time to press on the ladybug, increasing hardness.
+		setTime(time - 100)
+		timeout();
+	}
+
+	const newTry = () => {
+		setFail(false);
+		setScore(0);
+		setTime(3100);
+	}
+
+	if (!fail) {
+		return (
+			<div className='App'>
+				{score > 0 && <div className='timeDisplay'>Time: {time/1000} sec</div>}
+				<div className='ladybugContainer'
+					onClick={clicked}
+					style={{ top: randomTop, left: randomLeft }}>
+					<img src={ladybug} className='ladybug' alt='ladybug' width='30px' />
+				</div>
+			</div>
+		)
+	} else {
+		scream.play()
+		return (
+			<div>
+				<h1>FAIL</h1>
+				<h2>Score: {score}</h2>
+				<img src={ladybug} className='ladybugBig' alt='ladybug' width='80%' onClick={() => newTry()} />
+			</div>);
+	}
+}
+
+export default App;
